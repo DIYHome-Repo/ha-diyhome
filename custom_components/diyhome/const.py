6d5c597e61d74301cfd5d69cb51b1db3b3934178
DIYHome <infra@diyhome.it>
2026-06-02 19:09:26 +0200

    fix(ha): v1.9.0 — const.py

--- custom_components/diyhome/const.py
+++ custom_components/diyhome/const.py
@@ -5,8 +5,3 @@ DOMAIN = "diyhome"
 PLATFORMS = ["switch", "sensor", "binary_sensor"]
 
 CLOUD_URL = "https://diyhome.cloud"
-OAUTH2_AUTHORIZE = f"{CLOUD_URL}/oauth/authorize"
-OAUTH2_TOKEN = f"{CLOUD_URL}/api/ha/oauth/token"
-
-OAUTH2_CLIENT_ID = "diyhome-ha"
-OAUTH2_CLIENT_SECRET = "diyhome-ha-2026"
